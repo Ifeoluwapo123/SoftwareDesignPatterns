442985ca673633554ab5522724524c54d4af5e37
adenusidamilola5@gmail.com <adenusidamilola5@gmail.com>
2021-08-04 12:49:45 +0100

    the abstract class

--- src/Template/Task.java
+++ src/Template/Task.java
@@ -1,2 +1,22 @@
-package Template;public class Task {
+package Template;
+
+public abstract class Task {
+    private  AuditTrail auditTrail;
+
+    public Task() {
+        this.auditTrail = new AuditTrail();
+    }
+
+    public Task(AuditTrail auditTrail) {
+        this.auditTrail = auditTrail;
+    }
+
+    public void execute(){
+        auditTrail.record();
+
+        doExecute();
+    }
+
+    protected abstract void doExecute();
+
 }
